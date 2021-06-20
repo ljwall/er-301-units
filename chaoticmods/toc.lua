b38444d077dde4f7485af00f4b0c0cf2f614e455
liam <liam.wall@gmail.com>
2021-06-20 20:58:41 +0100

    Rename category

--- chaoticmods/toc.lua
+++ chaoticmods/toc.lua
@@ -7,13 +7,13 @@ return {
       -- Title and category control how this unit appears in the unit browser.
       title = "Logistic Map",
       moduleName = "LogisticMap",
-      category = "Chaotic Modulation",
+      category = "Chaos",
     },
     {
       -- Title and category control how this unit appears in the unit browser.
       title = "Lorenz System",
       moduleName = "Lorenz",
-      category = "Chaotic Modulation",
+      category = "Chaos",
     },
   }
 }
